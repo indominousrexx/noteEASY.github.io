1cf79cf6aa6e57fcaf705ad5aa8aeaa3706c2f62
indominousrexx <65479911+indominousrexx@users.noreply.github.com>
2020-05-25 17:06:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,17 +2,19 @@
 showNotes();
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function(e) {
-  let addTxt = document.getElementById("addTxt");
+ let addTxt = document.getElementById("addTxt");
+ let addTxt2 = document.getElementById("addTxt2");
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
   }
-  notesObj.push(addTxt.value);
-  localStorage.setItem("notes", JSON.stringify(notesObj));
+  
+  notesObj.push(addTxt2.value + '<hr id="hrj" />' + addTxt.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj)); 
   addTxt.value = "";
-
+  addTxt2.value = "";
   showNotes();
 });
 
@@ -26,7 +28,7 @@ function showNotes() {
   let html = "";
   notesObj.forEach(function(element, index) {
     html += `
-            <div class="noteCard my-2 mx-2 card" id="notecard" style="width: 18rem;">
+            <div class="noteCard my-3 mx-2 card" id="notecard" style="width: 18rem;">
                     <div class="card-body">
                         <h5 class="card-title">Note ${index + 1}</h5>
                         <p class="card-text"> ${element}</p>
